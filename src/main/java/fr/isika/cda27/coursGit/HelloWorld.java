cbb41e180f9d3d25d1bb25a65ef7aa0e89ce57b3
Elvariand <95416175+Elvariand@users.noreply.github.com>
2024-07-24 17:13:34 +0200

    Tout premier commit

---
+++ src/main/java/fr/isika/cda27/coursGit/HelloWorld.java
@@ -0,0 +1,10 @@
+package fr.isika.cda27.coursGit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
